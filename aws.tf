fc9368054f5ed311c7ab26e9f186ef1063abc01f
Keen <keen.ballon@seven-gen.com>
2025-06-20 14:21:16 +0800

    update

--- aws.tf
+++ aws.tf
@@ -1,41 +1,30 @@
-# provider "aws" {
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_key
-#   token      = var.aws_token
+  region = "us-east-1"
+}
 
-#   region = "us-east-1"
-# }
+resource "aws_s3_bucket" "b1" {
+  bucket = "s3-resume-bucket-lab"
 
-# resource "aws_s3_bucket" "b1" {
+  acl = "private" # or can be "public-read"
 
-#   bucket = "s3-resume-bucket-lab"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+  force_destroy = true
+}
 
-#   acl = "private" # or can be "public-read"
+# Upload an object
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.b1.id
+  key    = "Resume.pdf"
+  acl    = "public-read" # or can be "public-read"
 
-#   tags = {
-
-#     Name = "My bucket"
-
-#     Environment = "Dev"
-
-#   }
-#   force_destroy = true
-
-
-# }
-
-# # Upload an object
-# resource "aws_s3_bucket_object" "object" {
-
-#   bucket = aws_s3_bucket.b1.id
-
-#   key = "Resume.pdf"
-
-#   acl = "public-read" # or can be "public-read"
-
-#   source       = "Resume.pdf"
-#   content_type = "application/pdf"
-#   etag         = filemd5("Resume.pdf")
-#   storage_class = "ONEZONE_IA"
-# }
+  source       = "Resume.pdf"
+  content_type = "application/pdf"
+  etag         = filemd5("Resume.pdf")
+  storage_class = "ONEZONE_IA"
+}
